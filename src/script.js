73f2519fecaf060c774055a2fa6be1fddf1d7548
Sara Roseira <42982497+sararoseira@users.noreply.github.com>
2023-05-15 12:23:17 +0100

    added hours digits

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
 "use strict";
 
+// changing the date
+
 let date = document.querySelector(".text h2");
 
 let baseDate = new Date();
 let realWeekDay = baseDate.getDay();
 let realTimeH = baseDate.getHours();
+if (realTimeH < 10) {
+  realTimeH = `0${realTimeH}`;
+}
 let realTimeM = baseDate.getMinutes();
 let weekDays = [
   "Sunday",
@@ -19,6 +24,8 @@ let weekDays = [
 realWeekDay = weekDays[baseDate.getDay()];
 date.innerHTML = `${realWeekDay}, ${realTimeH}:${realTimeM}`;
 
+// displaying the city input
+
 let form = document.querySelector(".input-group");
 
 const submitFunc = function (event) {
@@ -28,3 +35,21 @@ const submitFunc = function (event) {
 };
 
 form.addEventListener("submit", submitFunc);
+
+// adding a celsius/fahrenheit option
+
+let temperature = document.querySelector("#temp");
+let celciusTemp = document.querySelector("#celsius");
+let fahrTemp = document.querySelector("#fahrenheit");
+
+const changingTemp = function () {
+  temperature.innerHTML = "61";
+};
+
+fahrTemp.addEventListener("click", changingTemp);
+
+const changingTemp2 = function () {
+  temperature.innerHTML = "19";
+};
+
+celciusTemp.addEventListener("click", changingTemp2);
